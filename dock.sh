3f945cd3a8ce11934f296e4b3066cb6638b5e124
iCeMan <fabiodiasfrancisco27@gmail.com>
2018-07-23 17:19:31 +0100

    fixed docker deployment

--- dock.sh
+++ dock.sh
@@ -4,7 +4,7 @@ lein clean
 if lein uberjar; then
     docker build -t contacts-api .
     echo "Docker image build successfully. Running..."
-    docker run -e CONTACTS_API_ENV=default  -it --rm -p 8080:8080  contacts-api
+    docker run -e CONTACTS_API_ENV=default -it --rm -p 8080:8080  contacts-api
 else
     echo "Cannot compile"
 fi
